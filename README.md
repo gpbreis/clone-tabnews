97e5f70f20b771191766432b6fc63496bfa6f648
Gustavo Pereira Breis <gpbreis@gmail.com>
2023-12-09 01:48:30 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um projeto para aprender programação desenvolvendo um clone do https://www.tabnews.com.br/
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Acabei de adicionar um novo recurso que vai deixar todo mundo feliz. Ou não. 🚀</h1>
+  return (
+    <h1>
+      Acabei de adicionar um novo recurso que vai deixar todo mundo feliz. Ou
+      não. 🚀
+    </h1>
+  );
 }
 
 export default Home;
